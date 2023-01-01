6ef214ae640380b0886c90d243794385d7bd4ace
Siddharth Choudhury <105007720+Sidhacker123@users.noreply.github.com>
2023-01-01 23:15:16 +0530

    Add files via upload
    Angle measurement in 3D via 4D Quaternion

--- Source.cpp
+++ Source.cpp
@@ -16,7 +16,6 @@ struct Point
 
 void displayPoints(vector<Point>& pointG, vector<Point>& pointR, vector<Point>& pointAns)
 {
-	cout << "sis";
 	osg::ref_ptr<osg::Vec3Array> polygonVertices = new osg::Vec3Array;
 	osg::ref_ptr<osg::Vec3Array> insideVertices = new osg::Vec3Array;
 	//osg::ref_ptr<osg::Vec3Array> onsideVertices = new osg::Vec3Array;
@@ -29,18 +28,18 @@ void displayPoints(vector<Point>& pointG, vector<Point>& pointR, vector<Point>& 
 	for (int i = 0; i < pointG.size(); i++)
 	{
 		polygonVertices->push_back(osg::Vec3(pointG[i].x, pointG[i].y, pointG[i].z));
-		colors->push_back(osg::Vec4(0.0f, 0.0f, 1.0f, 1.0f));
+		colors->push_back(osg::Vec4(1.0f, 1.0f, 1.0f, 1.0f));
 	}
 	for (int i = 0; i < pointR.size(); i++)
 	{
 		insideVertices->push_back(osg::Vec3(pointR[i].x, pointR[i].y, pointR[i].z));
-		colors1->push_back(osg::Vec4(1.0f, 0.0f, 0.0f, 1.0f));
+		colors1->push_back(osg::Vec4(1.0f, 1.0f, 1.0f, 1.0f));
 	}
 
 	for (int i = 0; i < pointAns.size(); i++)
 	{
 		outsideVertices->push_back(osg::Vec3(pointAns[i].x, pointAns[i].y, pointAns[i].z));
-		colors2->push_back(osg::Vec4(0.0f, 1.0f, 0.0f, 1.0f));
+		colors2->push_back(osg::Vec4(1.0f, 1.0f, 1.0f, 1.0f));
 	}
 
 	normals->push_back(osg::Vec3(0.0f, -1.0f, 0.0f));
@@ -99,7 +98,22 @@ void displayPoints(vector<Point>& pointG, vector<Point>& pointR, vector<Point>& 
 	viewer.run();
 
 }
-double anglex(Point Y) {
+
+double angleXY(Point Y, Point X) {       //angle btw 2 vectors
+	double val = (Y.x * X.x) + (Y.y * X.y) + (Y.z * X.z);
+
+	double val1 = (Y.x * Y.x) + (Y.y * Y.y) + (Y.z * Y.z);
+	double val2 = (X.x * X.x) + (X.y * X.y) + (X.z * X.z);
+
+	double val3 = sqrt(val1);
+	double val4 = sqrt(val2);
+	double val5 = val / (val1 * val2);
+	double angleX = acos(val5);
+	angleX = angleX * (180 / 3.1415);
+	return angleX;
+}
+
+/* double anglex(Point Y) {
 	double val = (Y.x * Y.x) + (Y.y * Y.y) + (Y.z * Y.z);
 	double val1 = sqrt(val);
 	double val2 = Y.x / val1;
@@ -107,6 +121,8 @@ double anglex(Point Y) {
 	angleX = angleX * (180 / 3.1415);
 	return angleX;
 }
+
+
 
 double angley(Point Y) {
 	double val = (Y.x * Y.x) + (Y.y * Y.y) + (Y.z * Y.z);
@@ -185,7 +201,7 @@ void rotatez(Point& X, Point Y, double angleDZ) {
 	//X.y = round(X.y);
 	//X.x = round(X.x);
 }
-
+*/
 int main()
 {
 	double a, b, c, d, e, f, g, h, i, j, k, l;
@@ -218,176 +234,56 @@ int main()
 	j = j - g;
 	k = k - h;
 	l = l - i;
+	double mag = (j * j) + (k * k) + (l * l);
+	mag = sqrt(mag);
 
 	Point Y = { j,k,l }; //ref
 
-	double anY = anglex(Y);
-	double anX = anglexg(X);
-	//cout << anY << " angle " << anX;
-	anY = round(anY);
-	anX = round(anX);
-	if (anY != anX) {
-		double del = anX - anY;
-		del = abs(del);
-		rotatey(X, Y, del);
-		double aa = anglex(Y);
-		double bb = anglexg(X);
-		aa = round(aa);
-		bb = round(bb);
-		if (aa != bb) {
-			del = 360 - (2 * del);
-			rotatey(X, Y, del);
-			aa = anglex(Y);
-			bb = anglexg(X);
-			aa = round(aa);
-			bb = round(bb);
-			if (aa != bb) {
-				del = anX - anY;
-				del = abs(del);
-				rotatey(X, Y, del);
-			}
-		}
-	}
-	if (anY != anX) {
-		double del = anX - anY;
-		del = abs(del);
-		rotatez(X, Y, del);
-		double aa = anglex(Y);
-		double bb = anglexg(X);
-		aa = round(aa);
-		bb = round(bb);
-		if (aa != bb) {
-			del = 360 - (2 * del);
-			rotatez(X, Y, del);
-			aa = anglex(Y);
-			bb = anglexg(X);
-			aa = round(aa);
-			bb = round(bb);
-			if (aa != bb) {
-				del = anX - anY;
-				del = abs(del);
-				rotatez(X, Y, del);
-			}
-		}
-	}
+	double anXY = angleXY(Y, X);
+	anXY = anXY / 2;
+	anXY = anXY * 3.14159 / 180;
+	double r = cos(anXY);
+	double si = sin(anXY);
+	double ii = -i;
+	vector<int> q;
+	q.push_back(r);
+	q.push_back(si * (j / mag));
+	q.push_back(si * (k / mag));
+	q.push_back(si * (l / mag));
 
-	cout << X.y << " y Angle with XX  z " << X.z << endl;
+	vector<int> qi;
+	qi.push_back(r);
+	qi.push_back(ii * (j / mag));
+	qi.push_back(ii * (k / mag));
+	qi.push_back(ii * (l / mag));
+
+	vector<int> p;
+	p.push_back(a);
+	p.push_back(b);
+	p.push_back(c);
+
+	double ci = (q[0] * p[0]) + (p[2] * q[2]) - (q[3] * p[1]);
+	double cj = (q[0] * p[1]) + (q[3] * p[0]) - (p[2] * q[1]);
+	double ck = (q[0] * p[2]) + (p[1] * q[1]) - (p[0] * q[2]);
+	double cr = (q[1] * p[0]) + (q[2] * p[1]) + (q[3] * p[2]);
+
+	double fi = ((ci * qi[0]) + (cj * qi[3]) + (cr * qi[1])) - ck * qi[2];
+	double fj = ((cj * qi[0]) + (ck * qi[1]) + (cr * qi[2])) - ci * qi[3];
+	double fk = ((ci * qi[2]) + (ck * qi[0]) + (cr * qi[3])) - cj * qi[1];
 
 
-	anY = angley(Y);
-	anX = angleyg(X);
-	anY = round(anY);
-	anX = round(anX);
-	//cout << anY << "  angle " << anX;
-	if (anY != anX) {
-		double del = anX - anY;
-		del = abs(del);
-		cout << "angle is" << del << endl;
-		rotatex(X, Y, del);
-		cout << X.y << " y 2y  z " << X.z << endl;
-		double aa = angley(Y);
-		double bb = angleyg(X);
-		aa = round(aa);
-		bb = round(bb);
-		cout << aa << " vv " << bb << endl;
-		if (aa != bb) {
-			del = 360 - (2 * del);
-			cout << "angle is" << del << endl;
-			rotatex(X, Y, del);
-			aa = angley(Y);
-			bb = angleyg(X);
-			aa = round(aa);
-			bb = round(bb);
-			if (aa != bb) {
-				del = aa - bb;
-				del = abs(del);
-				cout << "angle is" << del << endl;
-				rotatex(X, Y, del);
-			}
-		}
-	}
-	if (anY != anX) {
-		double del = anX - anY;
-		del = abs(del);
-		cout << "angle is" << del << endl;
-		rotatez(X, Y, del);
-		cout << X.y << " y 2y  z " << X.z << endl;
-		double aa = angley(Y);
-		double bb = angleyg(X);
-		aa = round(aa);
-		bb = round(bb);
-		if (aa != bb) {
-			del = 360 - (2 * del);
-			cout << "angle is" << del << endl;
-			rotatez(X, Y, del);
-			if (angley(Y) != angleyg(X)) {
-				del = anX - anY;
-				del = abs(del);
-				cout << "angle is" << del << endl;
-				rotatez(X, Y, del);
-			}
-		}
-	}
-	cout << "angle with y over" << endl;
 
-	anY = anglez(Y);
-	anX = anglezg(X);
-	anY = round(anY);
-	anX = round(anX);
-	//cout << anY << "  angle " << anX;
-	if (anY != anX) {
-		double del = anX - anY;
-		del = abs(del);
-		cout << "angle is" << del << endl;
-		rotatex(X, Y, del);
-		cout << X.y << "y 3z  z" << X.z << endl;
-		double aa = anglez(Y);
-		double bb = anglezg(X);
-		aa = round(aa);
-		bb = round(bb);
-		if (aa != bb) {
-			del = 360 - (2 * del);
-			rotatex(X, Y, del);
-			aa = anglez(Y);
-			bb = anglezg(X);
-			aa = round(aa);
-			bb = round(bb);
-			if (aa != bb) {
-				del = anX - anY;
-				del = abs(del);
-				rotatex(X, Y, del);
-			}
-		}
-	}
-	if (anY != anX) {
-		double del = anX - anY;
-		del = abs(del);
-		cout << "angle is" << del << endl;
-		rotatey(X, Y, del);
-		cout << X.y << "y 3z  z" << X.z << endl;
-		double aa = anglez(Y);
-		double bb = anglezg(X);
-		aa = round(aa);
-		bb = round(bb);
-		if (aa != bb) {
-			del = 360 - (2 * del);
-			rotatey(X, Y, del);
-			aa = anglez(Y);
-			bb = anglezg(X);
-			aa = round(aa);
-			bb = round(bb);
-			if (aa != bb) {
-				del = anX - anY;
-				del = abs(del);
-				rotatey(X, Y, del);
-			}
-		}
-	}
+
+
+
+
+
+
 
 	//cout << X.x<<"x   "<<X.y << " y final  z " << X.z;
-	R = { X.x,X.y,X.z };
+	R = { fi,fj,fk };
 	pointAns.push_back(R);
-	cout << X.x << " " << X.y << " " << X.z;
+	cout << fi << " " << fj << " " << fk;
 	displayPoints(pointG, pointR, pointAns);
 
 }
